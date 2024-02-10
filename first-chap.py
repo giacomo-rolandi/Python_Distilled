4e1af30b0e271e9f05d1353c16a28f58366cfa36
Giacomo <giacomo.fabrizio.rolandi@gmail.com>
2024-02-10 22:14:51 +0100

    Stuff added

--- first-chap.py
+++ first-chap.py
@@ -1,2 +1,106 @@
 #Primo Capitolo del libro
 
+x = 7
+
+y = 4
+
+"""
+Stampa di apprx, div/mod , potenza
+
+print(abs(-(x/y)))
+print(divmod(x,y))
+print(pow(x, 2, 7))
+print(round(x/y))
+
+
+"""
+
+
+
+
+
+"""
+Nel primo caso stampa la stringa con le varie funzioni, nel secondo così com'è scritto
+N.B. Per selezionare più righe premere SHIFT, Per spostarle ALT, e per ricopiarle su o giù SHIFT+ALT
+
+print(str("\n \n \n"))
+print(repr("\n \n \n"))
+
+"""
+
+n1 = 9
+n2 = -23
+
+
+
+"""
+Stampa binario
+
+print(bin(n2))
+print(" " + bin(n1))
+print(bin(~n1))
+print(~n1)
+
+"""
+
+
+
+
+"""
+While con la funzione continue
+
+x = 10
+
+while x < 11:
+    x += 1
+    if x == 11:
+        print("dentro")
+        continue
+    print("ciao")
+
+print("fatto")
+
+"""
+
+
+
+
+"""
+Rimozione/scambio lettere in una stringa
+
+a= "Hello World "
+
+print(a.strip("ld ") + "c")
+
+"""
+
+
+
+
+"""
+
+
+Apre un file e stampa riga per riga 
+
+with open (r"C:\Users\angel\Desktop\Study_Method.txt") as file:
+    for line in file:
+        print (line, end="")
+                     Di base con end impostato a \n
+
+
+"""
+
+"""
+
+Variante senza with
+
+"""
+
+file = open (r"C:\Users\angel\Desktop\Study_Method.txt")
+
+for line in file:
+    print(line, end="")
+file.close()
+
+
+
